81418b97553e09205bffba732c814239be11ea9d
wungthing <wungthingkashungnao123@gmail.com>
2023-09-29 12:56:39 +0530

    completed design of signup page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import SignUp from "./pages/signup/SignUp";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SignUp />
+    </div>
+  );
 }
 
 export default App;
