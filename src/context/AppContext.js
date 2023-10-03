a97b0cb0e97310b5847ed65e0c98e70f56dc6619
wungthing <wungthingkashungnao123@gmail.com>
2023-10-03 23:22:58 +0530

    completed check list 2

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -6,19 +6,11 @@ const AppContext = (props) => {
   const [ctgData, setCtgData] = useState([]); //state to handle selected category data
   const [localData, setLocalDdata] = useState([]);
   useEffect(() => {
-    // if (ctgData.length === 0) {
-    //   const data = JSON.parse(localStorage.getItem("categories"));
-    //   setCtgData(data);
-    //   return;
-    // } else {
-    //   localStorage.setItem("categories", JSON.stringify(ctgData));
-    //   const data = JSON.parse(localStorage.getItem("categories"));
-    //   setLocalDdata(data);
-    // }
     localStorage.setItem("categories", JSON.stringify(ctgData));
     const data = JSON.parse(localStorage.getItem("categories"));
     setLocalDdata(data);
   }, [ctgData]);
+
   // function to add data into seletcted category state which being called from selelectedcategory page
   const handleCtgData = (data) => {
     const sameData = ctgData.find((item) => item.value === data.value);
@@ -26,16 +18,18 @@ const AppContext = (props) => {
       const newValue = data;
       newValue.isSelected = true;
       setCtgData([...ctgData, newValue]);
-      // localStorage.setItem("categories", JSON.stringify(ctgData));
+      localStorage.setItem("categories", JSON.stringify(ctgData));
     }
   };
+  // console.log("when added=>", ctgData);
 
   // function to delete data from selected category state
   const handleDelteCtg = (data) => {
     data.isSelected = false;
     const newList = ctgData.filter((item) => item.value !== data.value);
     setCtgData(newList);
-    // localStorage.setItem("categories", JSON.stringify(ctgData));
+    localStorage.setItem("categories", JSON.stringify(ctgData));
+    // console.log("when deleted=>", data);
   };
   return (
     <context.Provider
--- src/pages/selectCategory/SelectCategory.jsx
+++ src/pages/selectCategory/SelectCategory.jsx
@@ -4,7 +4,6 @@ import { useContext } from "react";
 import { context } from "../../context/AppContext";
 const SelectCategory = () => {
   const { localData, handleDelteCtg, ctgData } = useContext(context);
-
   const handleNextPage = () => {
     if (ctgData.length >= 3) {
       console.log("into next page");
