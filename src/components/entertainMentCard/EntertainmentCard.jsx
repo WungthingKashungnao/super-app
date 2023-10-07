4fa3a365f8d1734d5811b9f4c092b61a81e8bbdb
wungthing <wungthingkashungnao123@gmail.com>
2023-10-07 12:54:23 +0530

    completed entertainment page

--- src/components/entertainMentCard/EntertainmentCard.jsx
+++ src/components/entertainMentCard/EntertainmentCard.jsx
@@ -3,21 +3,28 @@ import styles from "./entertainment.module.css";
 import { moviesApi } from "../../utils/api";
 
 const EntertainmentCard = ({ genreMovie }) => {
+  const imgUrl = "https://image.tmdb.org/t/p/w1280";
   const [movies, setMovies] = useState(); //state to store api data
   useEffect(() => {
     fetchMovieApi(genreMovie);
   }, [genreMovie]);
 
+  // fetching data fromt the movies api
   const fetchMovieApi = (genre) => {
     moviesApi(genre).then((data) => {
       setMovies(data.results.slice(0, 4));
     });
   };
-  // console.log(movies?.results.slice(0, 4));
-  console.log(movies);
+
+  // here in this return statement im looping 4 movies of the same category
   return (
     <div className={styles.categoryList}>
-      {movies && movies?.map((data) => <span>{data.title}</span>)}
+      {movies &&
+        movies?.map((data) => (
+          <div className={styles.ctgImg} key={data.id}>
+            <img src={`${imgUrl}${data?.poster_path}`} alt="" />
+          </div>
+        ))}
     </div>
   );
 };
--- src/pages/entertainment/Entertainment.jsx
+++ src/pages/entertainment/Entertainment.jsx
@@ -1,26 +1,34 @@
 import styles from "./entertainment.module.css";
 import profilePicture from "./profilePicture.png";
 import EntertainmentCard from "../../components/entertainMentCard/EntertainmentCard";
+import { useNavigate } from "react-router-dom";
 
 const Entertainment = () => {
-  const genres = JSON.parse(localStorage.getItem("SavedCategories"));
-
+  const genres = JSON.parse(localStorage.getItem("SavedCategories")); //getting the categories saved by the user
+  const navigate = useNavigate();
   return (
     <div className={styles.container}>
       <div className={styles.header}>
-        <h2 className={styles.brand}>Super App</h2>
+        <h2 className={styles.brand} onClick={() => navigate("/homePage")}>
+          Super App
+        </h2>
         <div className={styles.userProfile}>
           <img src={profilePicture} alt="" />
         </div>
       </div>
       <div className={styles.entertainment}>
         <h2>Entertainment according to your choice</h2>
+        {/* here im looping for all the categories of the movie genres ---start--- */}
         <div className={styles.entertainmentCategories}>
-          {genres.map((data) => (
-            // <div key={data.value} className={styles.categoryList}></div>
-            <EntertainmentCard genreMovie={data.value} key={data.value} />
-          ))}
+          {genres &&
+            genres.map((data) => (
+              <div key={data.value}>
+                <h3>{data.label}</h3>
+                <EntertainmentCard genreMovie={data.value} key={data.value} />
+              </div>
+            ))}
         </div>
+        {/* here im looping for all the categories of the movie genres ---start--- */}
       </div>
     </div>
   );
